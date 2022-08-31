339a6201cd34db5325670da9155b88e963a84a90
alt2cl <aldodiaz1983@gmail.com>
2022-08-31 09:21:22 -0400

    last deply

--- pages/[lang]/index.js
+++ pages/[lang]/index.js
@@ -2,22 +2,22 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 import Header from '@/components/UI/Organismo/Header'
 
-export default function PostPage() {
+export default function PostPageLang() {
   const router = useRouter()
-  const {country} = router.query
+  const {lang} = router.query
 
   return (
     <>
       <Header />
-      <h1>Post: {country}</h1>
+      <h1>Post: {lang}</h1>
       <ul>
         <li>
-          <Link href={`/${country}/chile`}>
+          <Link href={`/${lang}/chile`}>
             <a>First comment</a>
           </Link>
         </li>
         <li>
-          <Link href={`/${country}/chile`}>
+          <Link href={`/${lang}/chile`}>
             <a>Second comment</a>
           </Link>
         </li>
