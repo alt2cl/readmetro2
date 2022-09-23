38bab56399ce39fd9d3aa773860b61da934314ae
El Knoppix <alejandro.munoz31@gmail.com>
2022-09-23 16:43:51 -0300

    test

--- components/Layout/layout.jsx
+++ components/Layout/layout.jsx
@@ -21,7 +21,7 @@ function Layout( {children} ) {
                 target="_blank"
                 rel="noopener noreferrer"
                 >
-                PowereX by{' '}
+                Powered by ->{' '}
                 <span>
                     <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
                 </span>
