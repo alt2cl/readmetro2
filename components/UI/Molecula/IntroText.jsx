11a4905326f4fa714dc81a0222d98de774b31caf
alt2cl <aldodiaz1983@gmail.com>
2022-12-12 17:54:50 -0300

    fix: agregando traduccion a mapas

--- components/UI/Molecula/IntroText.jsx
+++ components/UI/Molecula/IntroText.jsx
@@ -20,13 +20,13 @@ const IntroText = () => {
 
     return (
         <Box sx={{background: `linear-gradient(313deg, #146435, #2A9858)`, mb:'2rem', mt:'1rem', p:['1.5rem 1rem','2rem'], textAlign:'center', color: ((theme) => theme.palette.common.white), borderRadius:'4px'}} >
-            <Typography variant="h6" sx={{fontWeight: '600', fontFamily: '"Bitter", serif;', lineHeight: '1.3'}}>
+            <Typography variant="subtitle1" sx={{fontWeight: '600', fontFamily: '"Bitter", serif;', lineHeight: '1.3'}}>
                 {langData.listWords.introText.title}
             </Typography>
-            <Typography variant="h6" sx={{fontFamily: '"Bitter", serif;', lineHeight: '1.3'}}>
+            <Typography variant="subtitle1" sx={{fontFamily: '"Bitter", serif;', lineHeight: '1.3'}}>
                 {langData.listWords.introText.subtitle}
             </Typography>
-            <Typography variant="h6" sx={{fontWeight: '600', fontFamily: '"Bitter", serif;', lineHeight: '1.3'}}>
+            <Typography variant="subtitle1" sx={{fontWeight: '600', fontFamily: '"Bitter", serif;', lineHeight: '1.3'}}>
                 {langData.listWords.introText.excerpt}
             </Typography>
         </Box>
